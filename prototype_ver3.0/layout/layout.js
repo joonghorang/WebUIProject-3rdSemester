973d1e8cf87ffdf76b8e763dc27f872794cb86e5
shinyoung <sypark9307@naver.com>
2014-11-24 00:45:09 +0900

    layout_proto
    대강의 레이아웃 참고안.
    서버까지 해서 프로토타입 완성하기
    gallery + preview(file, text input) <-> output
    #16

--- prototype_ver3.0/layout/layout.js
+++ prototype_ver3.0/layout/layout.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function(){
     inputMenuControl();
+    selectFile();
 },false)
 
 function inputMenuControl(){
@@ -10,9 +11,22 @@ function inputMenuControl(){
             menu.style.display = 'block';
             menubutton.style.backgroundImage = 'url(./menu_button_close.png)';
         }
-        else{
+        else{ 
             menu.style.display = 'none';
             menubutton.style.backgroundImage = 'url(./menu_button_open.png)';
         }
+    },false);
+}
+
+/*팀 설명용 임시 효과. 파일 업로드 시 미리보기 창 정도의 느낌.*/
+function selectFile(){
+    var menu_bg = document.querySelector('.input_menu_bg');
+    var file = document.querySelector('input:nth-child(1)');
+    var preview = document.querySelector('.inputFilePreview');
+    
+    file.addEventListener('click', function(){
+        menu_bg.style.transition = 'transform 1s';
+        menu_bg.style.transform = 'scaleY(18)';
+        preview.style.display = 'block';
     },false);
 }
