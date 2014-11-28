cfb28c48b1b3c44b73eff05297de664db00fccc0
stuzeakd <stuzeakd@nhnnext.org>
2014-11-28 14:37:28 +0900

    proto 3 server making

---
+++ prototype_ver3.0/server-nodejs/requestHandlers.js
@@ -0,0 +1,14 @@
+var index = function(req, res){
+    res.render("index");
+}
+
+var upload = function(req, res){
+    
+}
+
+var colorLab = function(req, res){
+       
+}
+exports.index = index;
+exports.upload = upload;
+exports.colorLab = colorLab;
