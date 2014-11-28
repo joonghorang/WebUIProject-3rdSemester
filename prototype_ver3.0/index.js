3a4715b1c2d2ae96f97051b674e7f7822beaddc7
shinyoung <sypark9307@naver.com>
2014-11-29 03:21:48 +0900

    조금
    gridItem 클릭 시 확장(추후 output으로 연결)
    hover해결 #22

--- prototype_ver3.0/index.js
+++ prototype_ver3.0/index.js
@@ -1,6 +1,7 @@
 
 window.addEventListener('DOMContentLoaded', function(){
 
+    var wrapper = document.getElementById('item-factory-wrapper');
     var itemFactory = document.getElementById('item-factory');
     var bg = document.getElementById('item-factory-background');
     var itemFactoryOpen = document.querySelector('#item-factory-nav-open');
@@ -8,10 +9,12 @@ window.addEventListener('DOMContentLoaded', function(){
 
 /*itemFactory 열기*/
     itemFactoryOpen.addEventListener('click',function(){
-        setItemFactory('0.8', 'block', 'none', 'block', '#101010');
+        wrapper.style.display = 'block';
+        setItemFactory('0.9', 'block', 'none', 'block', '#101010');
 },false);
 /*itemFactory 닫기*/
     itemFactoryClose.addEventListener('click',function(){
+        wrapper.style.display = 'none';
         setItemFactory('0', 'none', 'block', 'none', 'transparent');
 },false);
 
@@ -23,6 +26,14 @@ window.addEventListener('DOMContentLoaded', function(){
         itemFactory.style.transition = 'all 0.8s ease-in-out';
         itemFactory.style.transform = 'scale(2.2)';
     },false);
+    
+/*gridItem 클릭 시 확대 - output페이지로*/
+    var gridItem = document.querySelector('.grid-item');
+    gridItem.addEventListener('click', function(){
+        gridItem.style.transition = 'all 0.5s ease-in-out';
+        gridItem.style.width = '100%';
+        gridItem.style.height = '100%';
+    } ,false);
     
 function setItemFactory(opacity, displayState, openBar, closeBar, bgColor){
     itemFactory.style.opacity = opacity;
