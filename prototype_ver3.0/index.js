888d82b111cbeef75cfd5a9dee274721a1b46181
shinyoung <sypark9307@naver.com>
2014-11-30 21:30:26 +0900

    prototype_ver3_complete
    중일+신영 코드 합치기 1차 완료

--- prototype_ver3.0/index.js
+++ prototype_ver3.0/index.js
@@ -16,7 +16,15 @@ window.addEventListener('DOMContentLoaded', function(){
         setItemFactory('0', 'none', 'block', 'none', 'transparent');
 },false);
 
-/*submit 클릭 시 itemFactory 확장 - output페이지로(html전환)*/
+    function setItemFactory(opacity, displayState, openBar, closeBar, bgColor){
+        itemFactory.style.opacity = opacity;
+        itemFactory.style.display = displayState;
+        itemFactoryOpen.style.display = openBar;
+        itemFactoryClose.style.display = closeBar;
+        bg.style.backgroundColor = bgColor;
+    }
+    
+/*confirm 클릭 시 itemFactory 확장 - text입력받기*/
     var confirm = document.getElementById('confirm-button');
     confirm.addEventListener('click',function(){
         itemFactory.style.backgroundColor = '#ffffff';
@@ -29,10 +37,10 @@ window.addEventListener('DOMContentLoaded', function(){
         itemFactory.style.height = '100%';
         
         var uploadButton = document.getElementById('upload-button');
-        uploadButton.style.transition = 'opacity 0.4s ease-in-out';
-        uploadButton.style.opacity = '0';
-        confirm.style.transition='opacity 0.4s ease-in-out';
-        confirm.style.opacity = '0';
+        uploadButton.style.transition = 'display 0.4s ease-in-out';
+        uploadButton.style.display = 'none';
+        confirm.style.transition='display 0.4s ease-in-out';
+        confirm.style.display = 'none';
         
         var submitButton = document.getElementById('submit-button');
         submitButton.style.transition = 'display 0.8s ease-in-out';
@@ -41,15 +49,56 @@ window.addEventListener('DOMContentLoaded', function(){
         var tempImg = document.getElementById('preview-image');
         tempImg.style.transition = 'display 0.8s ease-in-out';
         tempImg.style.display = 'none';
+        
+        var background = document.getElementById('back-ground-canvas');
+        var preview = document.getElementById('preview');
+        var textInput = document.getElementById('text-input');
+        textInput.style.display = 'block';
+        preview.style.width = '100%';
+        preview.style.height = '100%';
+        background.style.display = 'block';
     },false);
     
-function setItemFactory(opacity, displayState, openBar, closeBar, bgColor){
-    itemFactory.style.opacity = opacity;
-    itemFactory.style.display = displayState;
-    itemFactoryOpen.style.display = openBar;
-    itemFactoryClose.style.display = closeBar;
-    bg.style.backgroundColor = bgColor;
-}
+var submit = document.getElementById('submit-button');
+    submit.addEventListener('click',function(){
+        
+        textValue = textInput.textContent;
+		textInput.style.display = "none";
+		submitButton.style.display = "none";
+        outputCanvas.style.display = "block";
+        backGroundCanvas.style.display='none';
+        
+        function getHour(){
+            var now = new Date();
+            var hour = now.getHours();
+            if (("" + hour).length == 1) { hour = "0" + hour; }
+                return hour;
+            return getCurrentTime().substr(8,2);
+        }
+        
+        var presentTime = parseInt(getHour());
+        if(presentTime < 10 || presentTime > 6 ){
+            fontName = "NanumMyeongjo";
+        } else {
+            fontName = "NanumBarunGothic";
+        }
+        
+        textWriter(); // 글자 쏴주는 함수 
+        
+        outputCanvas.style.transition = 'opacity 2s ease-in-out';
+        outputCanvas.style.opacity = '0';
+        setTimeout(function(){
+            wrapper.style.display = 'none';
+        },2000);
+        
+        itemFactoryOpen.style.display = 'block';
+        itemFactoryClose.style.display = 'none';
+
+        /*TODO preview-image 의 자식 노드를 지우기*/
+        
+        
+    },false);
+
     
 /*gridItem 클릭 시 확대 - output페이지로*/
     var gridItem = document.querySelector('.grid-item');
@@ -59,6 +108,7 @@ function setItemFactory(opacity, displayState, openBar, closeBar, bgColor){
         gridItem.style.height = '100%';
     } ,false);    
   
+    
 /*파일 선택시 선택한 이미지를 preview에 보여주기*/
     var fileInput = document.getElementById('upload-hidden');
     fileInput.addEventListener('change', function(){
