3c7cf56a2533628f5f0ed7d2e0d8fb17642feba7
shinyoung <sypark9307@naver.com>
2014-12-11 20:29:04 +0900

    server
    ajax

--- Ver1.0/ver1.0_server/server.js
+++ Ver1.0/ver1.0_server/server.js
@@ -34,19 +34,20 @@ app.post('/upload', function(request, response){
                
         if(error){
             console.log('form parsing error');
-            response.redirect('/error');
+            throw error;
         }
         else{
             //readFile : 업로드된 파일을 tmp디렉토리에 저장한다.
-            fs.readFile(files.image.path, function(error, data){
+            console.log(files, fields);
+            fs.readFile(files.inputImage.path, function(error, data){
                 
                 //writeFile : tmp디렉토리에 저장된 파일을 다른 디렉토리로 복사(저장)해준다.
                 //파일을 저장할 경로 설정. image/uploads 폴더 안에 uploadImage의 이름대로 저장된다. 
-                var uploadFileName = __dirname + '/image/uploads/' + files.image.name;
+                var uploadFileName = __dirname + '/image/uploads/' + files.inputImage.name;
                 fs.writeFile(uploadFileName, data, function(error){
                     if(error){
                         console.log('file saving error');
-                        response.redirect('/error');
+                        throw error;
                     }
                     else {
                         response.redirect('/output');
