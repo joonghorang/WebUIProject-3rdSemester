f342b678d61a064a6b254a48c011f0a2974a55f2
shinyoung <sypark9307@naver.com>
2014-12-13 00:51:12 +0900

    solve crash problem
    server skeleton complete
    DB connection started
    need color logic

--- Ver1.0/mainserver/server.js
+++ Ver1.0/mainserver/server.js
@@ -2,9 +2,23 @@
 var express = require('express');
 var formidable = require('formidable'); 
 var fs = require('fs');
-
-// localDB가 아니라 서버에 저장되어있는 DB로 하려면....?
+var ejs = require('ejs');
 var mysql = require('mysql');
+
+//express 모듈을 사용해 웹서버를 생성한다.
+var app = express();
+
+//static폴더 위치를 찾는다.
+var static = __dirname + '/static';
+app.use(express.static(static)); 
+
+//view engine은 ejs로 설정한다. view engine=template engine (ex)jade, ejs, mustache
+app.set('view engine', 'ejs');
+//views(template파일들이 위치하는 폴더)를 지정한다.
+app.set('views', __dirname + '/view');
+app.engine('html', require('ejs').renderFile);
+
+//DB connect : .db로 추후 수정
 var connection = mysql.createConnection({
  host :'localhost',
  user : 'joong',
@@ -19,26 +33,24 @@ connection.connect(function(err){
  }
 });
 
-//express 모듈을 사용해 웹서버를 생성한다.
-var app = express();
-
-//static폴더 위치를 찾는다.
-var static = __dirname + '/static';
-app.use(express.static(static)); 
-
-app.set('views', __dirname + '/view');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response){
-    response.render('index.html');
+    
+    /*DB SELECT : data for momentsBar(color)*/
+    /*//DB SELECT : data for momentsBar*/
+    
+//    response.render('main',data);
 });
 
 app.get('/output', function(request, response){
-    response.render('output.html');
+    
+    /*DB SELECT : all data(bgImg, img, color, text, date)*/
+    /*//DB SELECT : all data(bgImg, img, color, text, date)*/
+    
+//    response.render('output',data);
 });
 
-app.post('/upload', function(request, response){
+//fileInput에서 받아온 데이터 처리
+app.post('/upload-image', function(request, response){
     //formidable. parse uploaded file.
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
@@ -48,10 +60,9 @@ app.post('/upload', function(request, response){
             throw error;
         }
         else{
-            console.log(files);
             //readFile : 업로드된 파일을 tmp디렉토리에 저장한다.
             fs.readFile(files.image.path, function(error, data){
-
+                
                 //writeFile : tmp디렉토리에 저장된 파일을 다른 디렉토리로 복사(저장)해준다.
                 //파일을 저장할 경로 설정. image/uploads 폴더 안에 uploadImage의 이름대로 저장된다. 
                 var uploadFileName = __dirname + '/uploads/' + files.image.name;
@@ -61,8 +72,14 @@ app.post('/upload', function(request, response){
                         throw error;
                     }
                     else {
-                        // colorLab로직 
-                        // 그리고 받은 데이터 전부를 DB에 삽입.
+                        /*colorLab로직 : 더크성크 tools로직 사용 - 2-3개 색 JSON으로 뱉어낸다*/
+//                        var RGB = {
+//                            "colorA" : "#FFFFFF",
+//                            "colorB" : "#FFFFFF"
+//                        };
+                        /*//colorLab로직*/
+                        
+                        /*DB INSERT : imageFile path + date + RGB data*/    
                         var d = new Date();
                         var dateTime = d.getFullYear() + '-'
                                         + d.getMonth() + '-'
@@ -74,17 +91,25 @@ app.post('/upload', function(request, response){
                         connection.query('INSERT INTO filePath (path, date) VALUES(' //+ PID + ',' 
                                                                         + '"' + files.image.path.toString() + '"' 
                                                                         + ',"' + files.image.lastModifiedDate.toString() + '");', function(err, res){
-                            if(err) throw err;
+                            if(err) {
+                                throw err;
+                            }
                            // console.log(res);
                         });
+                        
                         connection.query('SELECT * FROM filePath', function(err, mysqlRes){
-                            if(err) throw err;
+                            if(err) {
+                                throw err;
+                            }
                             var stringifyResult = JSON.stringify(mysqlRes);
                             response.send(stringifyResult);
                             console.log(stringifyResult);
                         });
-                        console.log('redirecting');
-//                        response.redirect('/output');
+                        /*//DB INSERT*/
+                        
+                        //클라이언트에 RGBdata 전달. 
+                        //textInput페이지 배경을 위한 colorData만 넘기면 되는 거죠? 나머지는 DB INSERT하면 끝.
+                        response.end(JSON.stringify({"rgb" : "#FFFFFF"}));
                     }
                 });
             });
@@ -92,28 +117,26 @@ app.post('/upload', function(request, response){
     });
 });
 
-// 여기 코드 합쳐주세요. 
-// app.post('/rgbDB', function (req, res){
-
-//  var form =  new formidable.IncomingForm();
-//  form.parse(req, function(err, fields, files){
-//         if(err){
-//             console.log(err);
-//         }else{
-//          connection.query('insert into RGBA values(' + fields.r + ','
-//                                                                  + fields.g + ','
-//                                                                  + fields.b + ','
-//                                                                  + fields.a + ');', function(err, result){
-//              if(err) throw err;
-//          });
-//             connection.query('SELECT * FROM RGBA' , function(err, result){
-//                 if(err) throw err;
-//                 var stringifyResult = JSON.stringify(result);
-//                 res.send(stringifyResult);
-//             });
-//         }
-//     });
-// });
+//textInput에서 받아온 데이터 처리
+app.post('/upload-text', function(request, response){
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files){
+    
+        if(error){
+            console.log('form parsing error');
+            throw error;
+        }
+        else{
+            //클라이언트에서 입력한 text data
+            var text = fields.textInput;
+            
+            /*DB INSERT : text*/
+            /*//DB INSERT : text*/
+            
+            response.end();
+        }
+    });
+});
 
 //웹서버를 실행한다.
 app.listen(3000, function(){
--- Ver1.0/mainserver/static/js/index.js
+++ Ver1.0/mainserver/static/js/index.js
@@ -4,34 +4,57 @@ var MAX_WIDTH = window.innerWidth;
 var navBar = document.getElementById("navBar");
 var momentsBar = document.getElementById("momentsBar");
 var fileInput = document.getElementById("upload-hidden");
+var textInput = document.getElementById("text-input");
+var confirmButton = document.getElementById("confirm-button");
 var submitButton = document.getElementById("submit-button");
 
 window.addEventListener('DOMContentLoaded', function(){  
 	test_genOutputs();
 }, false);
 
-submitButton.addEventListener('click', function(e){
+//confirm버튼 누르면 이미지를 서버로 보내기.
+confirmButton.addEventListener('click', function(e){
 	e.preventDefault();
 
-	var formData = new FormData();
-    var localUrl = "/upload";
-
-    /*파일 없을때 에러처리*/
+    //파일 없을때 에러처리
     if(fileInput.files.item(0)===null){
         alert('no image');
     }
-    /*AJAX로 데이터 보내기*/
-    formData.append("image", fileInput.files[0]);
+    //AJAX로 데이터 받아오기
     var request = new XMLHttpRequest();
-	request.open("POST" , "/upload" , true);
-	request.send(formData);
+    var formData = new FormData();
+    formData.append("image", fileInput.files[0]);
+
+	//길을 열어라!
+    request.open("POST" , "/upload-image" , true);
+	//보내라!
+    request.send(formData);
+    //받아와라!(data가 load되면 실행)
+    request.addEventListener('load', function(){
+        //받아온 JSON
+        console.log(request.responseText);
+        var result = JSON.parse(request.responseText)
+        
+        //JSON에 있는 RGB데이터로 텍스트입력창 배경색 그리기(bgColor=result.rgb)
+        
+    });
     
-    /*AJAX로 데이터 받기*/
-    request.onreadystatechange = function() 
-    {
-       if (request.readyState == 4 && request.status == 200) 
-       {    
-           console.log(request.responseText);
-       }
-    }
+//    request.onreadystatechange = function(){
+//       if (request.readyState == 4 && request.status == 200){    
+//           console.log(request.responseText);
+//       }
+//    }
+    
 }, false);
+
+//submit버튼 누르면 텍스트 데이터를 서버로 보내기.
+submitButton.addEventListener('click', function(e){
+    e.preventDefault();
+    var request = new XMLHttpRequest();
+    var formData = new FormData();
+    formData.append("textInput", textInput.value);
+    
+    request.open("POST", "/upload-text", true);
+    request.send(formData);
+    
+},false);
