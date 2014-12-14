1bd4865641f2446398d569c5606b562f82d18b7b
shinyoung <sypark9307@naver.com>
2014-12-14 22:06:10 +0900

    various
    +ejs

--- Ver1.0/mainserver/server.js
+++ Ver1.0/mainserver/server.js
@@ -15,41 +15,38 @@ var app = express();
 var static = __dirname + '/static';
 app.use(express.static(static)); 
 
+//upload Img가져올 폴더 찾기 위한 static설정
+var uploads = __dirname + '/uploads';
+app.use(express.static(uploads));
+
 //view engine은 ejs로 설정한다. view engine=template engine (ex)jade, ejs, mustache
 app.set('view engine', 'ejs');
 //views(template파일들이 위치하는 폴더)를 지정한다.
 app.set('views', __dirname + '/view');
 app.engine('html', require('ejs').renderFile);
 
-//DB connect : .db로 추후 수정
-//var connection = mysql.createConnection({
-// host :'localhost',
-// user : 'joong',
-// password : 'db1004',
-// database : 'joongdb'
-//});
-//connection.connect(function(err){
-// if(err){
-//     console.error('mysql connection error');
-//     console.error(err);
-//     throw err;
-// }
-//});
+//DB connect
 
 app.get('/', function(request, response){
-    
     /*DB SELECT : data for momentsBar(color)*/
     /*//DB SELECT : data for momentsBar*/
+    var mainData = {
+        "color1" : "#ffffff",
+        "color2" : "#ffffff",
+        "color3" : "#ffffff"
+    };
     
-//    response.render('main',data);
+    response.render('main',mainData);
 });
 
 app.get('/output', function(request, response){
-    
     /*DB SELECT : all data(bgImg, img, color, text, date)*/
     /*//DB SELECT : all data(bgImg, img, color, text, date)*/
-    
-//    response.render('output',data);
+    var outputData = {
+        "image-src" : "./uploads/test.jpg"    
+    };
+
+    response.render('output',outputData);
 });
 
 //fileInput에서 받아온 데이터 처리(confirm상태) : 이미지 읽어서 colorData DB에 저장, 클라에 전달
@@ -66,7 +63,8 @@ app.post('/upload-image', function(request, response){
             //readFile : 업로드된 파일을 tmp디렉토리에 저장한다.
             fs.readFile(files.image.path, function(error, data){
                 /* 덕성 comment
-                Color 로직은 여기에 들어갑니다. readFile되었을 때 data를 읽고 적절한 칼라를 뽑아 pickedColors에 저장하고 send해줍니다.
+                Color 로직 
+                readFile되었을 때 data를 읽고 적절한 칼라를 뽑아 pickedColors에 저장하고 send해줍니다.
                 뽑은 pickedColors를 클라이언트로 보내는 작업과는 비동기적으로 writeFile을 진행합니다.
                 client가 받은 pickedColors가 json으로 잘 작동하지 않으면 
                 response.send(JSON.stringify(pickedColors));로 대체해서 시도해보세요.
@@ -79,6 +77,68 @@ app.post('/upload-image', function(request, response){
                 
                 var pickedColors = imgP.pickColors(canvas);
                 
+                /*DB INSERT : date + RGB data*/    
+                /*//DB INSERT*/
+                
+                response.send(pickedColors);
+                response.end();
+            });
+        }
+    });
+});
+
+//textInput에서 받아온 데이터 처리(submit상태) : 이미지 다시 읽어서 파일 최종 저장, 다른 데이터도 저장
+app.post('/upload-text', function(request, response){
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files){
+    
+        if(error){
+            console.log('form parsing error');
+            throw error;
+        }
+        else{
+            //클라이언트에서 입력한 text data
+            var text = fields.textInput;
+            var fileName = ;
+            var uploadFileName = __dirname + '/uploads/' + fileName;
+            
+            fs.readFile(files.image.path, function(error, data){
+                fs.writeFile(uploadFileName, data, function(error){
+                    if(error){
+                        console.log('file saving error');
+                        throw error;
+                    }
+                    else {
+                        /*DB INSERT : text, filePath*/
+                        /*//DB INSERT : text*/
+
+                        response.end();
+                    }
+                });
+            });
+        }
+    });
+});
+
+//웹서버를 실행한다.
+app.listen(3000, function(){
+    console.log('server running at port 3000...');
+});
+
+//DB connect : .db로 추후 수정
+//var connection = mysql.createConnection({
+// host :'localhost',
+// user : 'joong',
+// password : 'db1004',
+// database : 'joongdb'
+//});
+//connection.connect(function(err){
+// if(err){
+//     console.error('mysql connection error');
+//     console.error(err);
+//     throw err;
+// }
+//});
                 /*DB INSERT : date + RGB data*/    
 //                        var d = new Date();
 //                        var dateTime = d.getFullYear() + '-'
@@ -105,49 +165,4 @@ app.post('/upload-image', function(request, response){
 //                            response.send(stringifyResult);
 //                            console.log(stringifyResult);
 //                        });
-//                        /*//DB INSERT*/
-                
-                response.send(pickedColors);
-                response.end();
-            });
-        }
-    });
-});
-
-//textInput에서 받아온 데이터 처리(submit상태) : 이미지 다시 읽어서 파일 최종 저장, 다른 데이터도 저장
-app.post('/upload-text', function(request, response){
-    var form = new formidable.IncomingForm();
-    form.parse(request, function(error, fields, files){
-    
-        if(error){
-            console.log('form parsing error');
-            throw error;
-        }
-        else{
-            //클라이언트에서 입력한 text data
-            var text = fields.textInput;
-            
-            fs.readFile(files.image.path, function(error, data){
-                var uploadFileName = __dirname + '/uploads/' + files.image.name;
-                fs.writeFile(uploadFileName, data, function(error){
-                    if(error){
-                        console.log('file saving error');
-                        throw error;
-                    }
-                    else {
-                        
-                        /*DB INSERT : text, filePath*/
-                        /*//DB INSERT : text*/
-
-                        response.end();
-                    }
-                });
-            });
-        }
-    });
-});
-
-//웹서버를 실행한다.
-app.listen(3000, function(){
-    console.log('server running at port 3000...');
-});
+//              /*//DB INSERT*/
