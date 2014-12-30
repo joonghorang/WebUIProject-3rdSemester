9deb4a06dbc9a58e5288b9436f6fd8a7a1848acf
Shinyoung <fairesy@gmail.com>
2014-12-31 01:40:37 +0900

    added some comments

--- Ver1.0/mainserver/static/js/newMain.js
+++ Ver1.0/mainserver/static/js/newMain.js
@@ -1,16 +1,20 @@
 var wrapper = document.getElementById('wrapper');
 var t2 = document.getElementById('t2');
 
-//e.stopPropagation을 어딘가에 적절히....?
-
-//body.addEventListener('mouseover', function(e){
-//    wrapper.style.backgroundColor = '#000000';
-//});
-
+//기본 구조
 t2.addEventListener('mouseover', function(){
     wrapper.style.backgroundColor = '#b1a686';
 });
 t2.addEventListener('mouseout', function(){
-    wrapper.style.transition = 'all 0.5s ease-in-out';
     wrapper.style.backgroundColor = 'transparent';
 });
+
+//아마도 이렇게 함수로 만들어서, span추가될때마다 색상값만 데이터로 넣어서 이벤트도 같이 추가하면 될 듯?
+function spanHover(targetSpan, bgColor){
+    targetSpan.addEventListener('mouseover', function(){
+    wrapper.style.backgroundColor = bgColor;
+    });
+    targetSpan.addEventListener('mouseout', function(){
+        wrapper.style.backgroundColor = 'transparent';
+    });
+};
